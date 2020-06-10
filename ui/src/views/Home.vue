cb170f96bc378cfb364420b6db572bcdf748f422
Filip <fdabek1@umbc.edu>
2020-06-10 11:03:40 -0400

    Add installation docs.

--- ui/src/views/Home.vue
+++ ui/src/views/Home.vue
@@ -3,12 +3,23 @@
     <div class="jumbotron">
       <h1 class="display-4">EHR Code Mappings</h1>
       <p class="lead">Welcome to EHR Code Mappings!</p>
+      <hr class="my-4">
+      <p>To install:</p>
+      <vue-code-highlight>
+        pip install ehr-code-mappings
+      </vue-code-highlight>
     </div>
   </div>
 </template>
 
 <script>
+  import {component as VueCodeHighlight} from 'vue-code-highlight';
+  import "vue-code-highlight/themes/prism-tomorrow.css";
+
   export default {
     name: 'Home',
+    components: {
+      VueCodeHighlight,
+    },
   }
 </script>
