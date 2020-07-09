999e46a3da9405ac2866a040c5acb736f11441c2
Filip <fdabek1@umbc.edu>
2020-07-08 22:00:21 -0400

    Ability to search filters.

--- ui/src/components/FilterBox.vue
+++ ui/src/components/FilterBox.vue
@@ -6,12 +6,12 @@
     <b-popover :target="title + '-target'" triggers="focus" placement="bottom">
       <template v-slot:title>{{ title }}</template>
       <b-card no-body class="filter-content overflow-auto p-2 border-0">
-        <b-input type="search" size="sm" class="mb-2"/>
+        <b-input type="search" size="sm" class="mb-2" v-model="search"/>
         <b-form-checkbox v-model="allSelected" :indeterminate="indeterminate" @change="toggleAll">
           {{ allSelected ? 'Select None' : 'Select All' }}
         </b-form-checkbox>
         <b-form-checkbox-group stacked v-model="filters">
-          <b-form-checkbox v-for="item in values" :value="item.title">
+          <b-form-checkbox v-for="item in searchedValues" :value="item.title">
             {{ item.title }} <span class="text-muted small">({{ item.count }})</span>
           </b-form-checkbox>
         </b-form-checkbox-group>
@@ -24,6 +24,11 @@
   export default {
     name: "FilterBox",
     props: ['title', 'values', 'value'],
+    data() {
+      return {
+        search: null,
+      }
+    },
     computed: {
       filters: {
         get() {
@@ -43,6 +48,13 @@
       },
       indeterminate() {
         return this.filters.length !== 0 && this.filters.length !== this.values.length;
+      },
+      searchedValues() {
+        console.log('searchedValues', this.search);
+        if (this.search === null || this.search === '')
+          return this.values;
+
+        return this.values.filter(value => value.title.toUpperCase().includes(this.search.toUpperCase()));
       },
     },
     methods: {
